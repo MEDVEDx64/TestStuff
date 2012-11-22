afed8c2fb15399b87714464b7ad6d8e059208f56
Alexey Dolzhenkov <leha-dolzhenkov@bk.ru>
2012-11-22 20:05:24 +0400

    Удалил ненужный код, отвечающий, за тестирование положения изображения перед отрисовкой, и вставил маленький код, который правильно выризает кусок из большой текстуры

--- draw.c
+++ draw.c
@@ -5,57 +5,18 @@
 
 void drawImage(t_Image *image, int x, int y, SDL_Rect *draw_rect)
 {
-    SDL_Rect pos;
-    pos.x = x;
-    pos.y = y;
-
-    SDL_Rect rect;
-    if (draw_rect) {
-        rect.x = draw_rect->x;
-        rect.y = draw_rect->y;
-        rect.w = draw_rect->w;
-        rect.h = draw_rect->h;
-    }
-    else {
-        rect.x = 0;
-        rect.y = 0;
-        rect.w = SDL_GetVideoSurface()->w;
-        rect.h = SDL_GetVideoSurface()->h;
-    }
-
     SDL_Rect r;
-    if (pos.x > rect.x) r.x = 0;
+    if (draw_rect) {
+        r.x = draw_rect->x;
+        r.y = draw_rect->y;
+        r.w = draw_rect->w;
+        r.h = draw_rect->h;
+    }
     else {
-        r.x = rect.x-pos.x;
-        pos.x += r.x;
-    }
-    if (pos.y > rect.y) r.y = 0;
-    else {
-        r.y = rect.y-pos.y;
-        pos.y += r.y;
-    }
-    if (pos.x-r.x+image->w < rect.x+rect.w) r.w = image->w;
-    else  {
-        int t = rect.x+rect.w-pos.x+r.x;
-        if (t < 0)  return;
-        else r.w = t;
-    }
-    if (pos.y-r.y+image->h < rect.y+rect.h) r.h = image->h;
-    else  {
-        int t = rect.y+rect.h-pos.y+r.y;
-        if (t < 0)  return;
-        else r.h = t;
-    }
-
-    if (r.x > 0) {
-        if (r.x > image->w) return;
-        else r.w-=r.x;
-    }
-
-
-    if (r.y > 0) {
-        if (r.y > image->h) return;
-        else r.h-=r.y;
+        r.x = 0;
+        r.y = 0;
+        r.w = image->w;
+        r.h = image->h;
     }
 
     glColor4f(1,1,1,1);
